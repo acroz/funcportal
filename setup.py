641505b44719091bd4de9b8fe9d5053f34c13ad5
Andrew Crozier <wacrozier@gmail.com>
2018-04-04 00:17:47 +0100

    Initialise version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(
     name='funcportal',
+    version='0.0.0',
     description='Serve Python functions as web APIs',
     packages=['funcportal'],
     install_requires=[
