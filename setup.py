9b9467e51eb6e70518e2102681b42a5acd2cdd00
Andrew Crozier <wacrozier@gmail.com>
2018-04-13 16:24:01 +0100

    Read long description from README

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+import os
 from setuptools import setup
 
 from funcportal.version import __version__
+
+
+def readme():
+    source_root = os.path.dirname(__file__)
+    path = os.path.join(source_root, 'README.rst')
+    with open(path) as fp:
+        return fp.read()
 
 
 setup(
     name='funcportal',
     version=__version__,
     description='Serve Python functions as web APIs',
+    long_description=readme(),
     packages=['funcportal'],
     install_requires=[
         'flask',
