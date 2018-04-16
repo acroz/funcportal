38cc0a6977cd856d52939499977627d14c57602a
Andrew Crozier <wacrozier@gmail.com>
2018-04-16 21:39:25 +0100

    Refactor common logic in handlers

--- funcportal/handler.py
+++ funcportal/handler.py
@@ -1,6 +1,7 @@
 import logging
 from collections import namedtuple
 import json
+from functools import wraps
 
 from flask import request, Response as FlaskResponse
 from werkzeug.exceptions import (
@@ -27,17 +28,60 @@ def _describe_arguments(arguments):
     }
 
 
+def handle_errors(handler_method):
+
+    @wraps(handler_method)
+    def wrapped(obj, *args, **kwargs):
+        try:
+            return handler_method(obj, *args, **kwargs)
+        except InvalidArgumentsError:
+            return Response(
+                400, {'error': 'Invalid arguments were passed.'}
+            )
+        except MissingArgumentsError as e:
+            data = {
+                'error': str(e),
+                'arguments': _describe_arguments(
+                    obj.portal_function.arguments
+                )
+            }
+            return Response(400, data)
+        except Exception:
+            logger.exception(
+                'Error evaluating the function {}'
+                .format(obj.portal_function.name)
+            )
+            return Response(500, {'error': 'Internal server error.'})
+
+    return wrapped
+
+
+def render_response(response, function_name):
+    status_code = response.status_code
+    try:
+        body = json.dumps(response.data)
+    except Exception:
+        logger.exception(
+            'Error serialising output from function {} as JSON'
+            .format(function_name)
+        )
+        status_code = 500
+        body = json.dumps({'error': 'Internal server error.'})
+    return body, status_code
+
+
 class BaseHandler(object):
 
     def __init__(self, portal_function):
         self.portal_function = portal_function
 
-    def _call_function(self, input_data):
-
+    def _call_and_catch_errors(self, callable):
         try:
-            output_data = self.portal_function(**input_data)
-        except InvalidArgumentsError as e:
-            return Response(400, {'error': 'Invalid arguments were passed.'})
+            return callable()
+        except InvalidArgumentsError:
+            return Response(
+                400, {'error': 'Invalid arguments were passed.'}
+            )
         except MissingArgumentsError as e:
             data = {
                 'error': str(e),
@@ -53,20 +97,9 @@ class BaseHandler(object):
             )
             return Response(500, {'error': 'Internal server error.'})
 
-        return Response(200, {'result': output_data})
-
-    def _render_response(self, response):
-        status_code = response.status_code
-        try:
-            body = json.dumps(response.data)
-        except Exception:
-            logger.exception(
-                'Error serialising output from function {} as JSON'
-                .format(self.portal_function.name)
-            )
-            status_code = 500
-            body = json.dumps({'error': 'Internal server error.'})
-        return body, status_code
+    @handle_errors
+    def _call_function(self, input_data):
+        return Response(200, {'result': self.portal_function(**input_data)})
 
 
 class FlaskHandler(BaseHandler):
@@ -80,11 +113,9 @@ class FlaskHandler(BaseHandler):
             inputs = {}
 
         response = self._call_function(inputs)
-        body, status_code = self._render_response(response)
 
-        return FlaskResponse(
-            body, status=status_code, mimetype='application/json'
-        )
+        body, status = render_response(response, self.portal_function.name)
+        return FlaskResponse(body, status=status, mimetype='application/json')
 
 
 class BaseQueueHandler(BaseHandler):
@@ -94,27 +125,14 @@ class BaseQueueHandler(BaseHandler):
         self.queue = queue
 
     def _submit_function(self, input_data):
-
-        try:
-            job_id = self.portal_function.submit(self.queue, **input_data)
-        except InvalidArgumentsError as e:
-            return Response(400, {'error': 'Invalid arguments were passed.'})
-        except MissingArgumentsError as e:
-            data = {
-                'error': str(e),
-                'arguments': _describe_arguments(
-                    self.portal_function.arguments
-                )
-            }
-            return Response(400, data)
-        except Exception:
-            logger.exception(
-                'Error evaluating the function {}'
-                .format(self.portal_function.name)
+        return self._call_and_catch_errors(
+            lambda: Response(
+                202,
+                {'job_id': self.portal_function.submit(
+                    self.queue, **input_data
+                )}
             )
-            return Response(500, {'error': 'Internal server error.'})
-
-        return Response(202, {'job_id': job_id})
+        )
 
 
 class FlaskQueueSubmissionHandler(BaseQueueHandler):
@@ -128,11 +146,9 @@ class FlaskQueueSubmissionHandler(BaseQueueHandler):
             inputs = {}
 
         response = self._submit_function(inputs)
-        body, status_code = self._render_response(response)
 
-        return FlaskResponse(
-            body, status=status_code, mimetype='application/json'
-        )
+        body, status = render_response(response, self.portal_function.name)
+        return FlaskResponse(body, status=status, mimetype='application/json')
 
 
 class FlaskQueueRetrievalHandler(BaseQueueHandler):
@@ -148,11 +164,8 @@ class FlaskQueueRetrievalHandler(BaseQueueHandler):
             output_data = job.result
             response = Response(200, {'result': output_data})
 
-        body, status_code = self._render_response(response)
-
-        return FlaskResponse(
-            body, status=status_code, mimetype='application/json'
-        )
+        body, status = render_response(response, self.portal_function.name)
+        return FlaskResponse(body, status=status, mimetype='application/json')
 
 
 def configure_flask_app(app):
